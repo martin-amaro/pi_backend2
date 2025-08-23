902a728a093c4ece22f92fa6bd8698f24da24de6
Martin Amaro <martingadpacks@gmail.com>
2025-08-22 22:13:26 -0500

    feat: add data.sql

--- src/main/java/com/example/pib2/controller/TestController.java
+++ src/main/java/com/example/pib2/controller/TestController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping
     public Map<String, String> test() {
+
         return Map.of("test", "ok");
     }
     
