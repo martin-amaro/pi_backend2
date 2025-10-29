d46d9cb11f4967ffcc9d9ea056a0ac795d5b4651
jonathan <luisalbertocaballero2024@gmail.com>
2025-10-28 22:26:48 -0500

    se arreglan los arreglos q se pidieron el otro dia

--- src/main/java/com/example/pib2/model/entity/InvoiceItem.java
+++ src/main/java/com/example/pib2/model/entity/InvoiceItem.java
@@ -6,11 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
 @Table(name = "invoice_items")
-@lombok.Data
+@Data
 public class InvoiceItem {
 
     @Id
@@ -19,19 +24,29 @@ public class InvoiceItem {
 
     @ManyToOne
     @JoinColumn(name = "invoice_id", nullable = false)
-    @lombok.ToString.Exclude
-    @lombok.EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private Invoice invoice;
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
-    @lombok.ToString.Exclude
-    @lombok.EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private Product product;
 
     private Integer quantity;
 
-    private Double unitPrice; // Precio al momento de la venta
+    private Double unitPrice;
 
-    private Double subtotal; // quantity * unitPrice
+    private Double subtotal;
+
+    @PrePersist
+    @PreUpdate
+    public void calculateSubtotal() {
+        if (this.quantity != null && this.unitPrice != null) {
+            this.subtotal = this.quantity * this.unitPrice;
+        } else {
+            this.subtotal = 0.0;
+        }
+    }
 }
